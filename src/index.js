a24888a6e5fa0e85e988d9d66fe832884a5e711f
synci <synci@yandex.ru>
2019-11-07 13:25:38 +0200

    Add Dino with animation.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import "../node_modules/phaser/dist/phaser.js"
+import World from "./modules/world"
 // import {config} from "./modules/config"
 // import AppService from "./modules/app.service"
-import {world} from "./modules/world"
+// import {world} from "./modules/world"
+
 
 // var service = new AppService('lol-kek');
 // service.log();
@@ -35,33 +37,34 @@ window.onload = function() {
 
     let game = new Phaser.Game(config);
     let platforms;
+    var dino;
+    let world;
 
     function preload ()
     {
         this.load.image('ground', '../assets/platform.png');
         this.load.image('star', '../assets/star.png');
+        this.load.spritesheet('dino', '../assets/dino_sprite.png', { frameWidth: 44, frameHeight: 46 });
 
         this.config = config;
+        world = new World(this);
         // this.load.image('sky', '../assets/sky.png');
         // this.load.image('bomb', '../assets/bomb.png');
-        // this.load.spritesheet('dude', '../assets/dude.png', { frameWidth: 32, frameHeight: 48 });
     }
 
 
     function create ()
     {
-        platforms = world.addPlatforms.call(this);
+        platforms = world.addPlatforms();
+        dino = world.addDino();
+
+        this.physics.add.collider(dino, platforms);
     }
 
     function update ()
     {
-        platforms.children.iterate(function (platform) {
-            let platformRightPosition = platform.getBounds().right;
-
-            if (platformRightPosition <= 0) {
-                platform.setX(platform.width + platformRightPosition);
-            }
-        });
+        dino.anims.play('run', true);
+        world.rotatePlatforms(platforms);
     }
 }
 
--- src/modules/world.js
+++ src/modules/world.js
@@ -1,17 +1,52 @@
-export const world = {
-    addPlatforms: function (count = 2) {
-        let platforms = this.physics.add.group({
+export default class World {
+    constructor(scene) {
+        this._scene = scene;
+    }
+
+    addPlatforms (count = 2) {
+
+        let platforms = this._scene.physics.add.group({
             immovable: true,
             allowGravity: false,
-            velocityX: this.config.custom.worldVelocity
+            velocityX: this._scene.config.custom.worldVelocity
         });
 
         for (let i = 0; i < count; i++) {
-            platforms.create(this.config.custom.platformSprite.w * i, 100, 'ground').setOrigin(0);
+            platforms.create(this._scene.config.custom.platformSprite.w * i,
+                this._scene.config.height - 50, 'ground').setOrigin(0);
         }
 
         return platforms;
-    }
-};
+    };
 
+    addDino() {
+        let dino = this._scene.physics.add.sprite(100, 100, 'dino');
+        dino.setBounce(0);
+        dino.setCollideWorldBounds(true);
+
+        this.addDinoAnimation();
+
+        return dino;
+    }
+
+    addDinoAnimation() {
+        this._scene.anims.create({
+            key: 'run',
+            frames: this._scene.anims.generateFrameNumbers('dino', { start: 0, end: 1 }),
+            frameRate: 5,
+            repeat: true
+        });
+    }
+
+    rotatePlatforms(platforms) {
+        platforms.children.iterate(function (platform) {
+            let platformRightPosition = platform.getBounds().right;
+
+            if (platformRightPosition <= 0) {
+                // player.anims.msPerFrame = 100;
+                platform.setX(platform.width + platformRightPosition);
+            }
+        });
+    }
+}
 
